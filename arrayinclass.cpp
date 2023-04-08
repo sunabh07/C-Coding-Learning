4a8f2a542909d98003c870a937cd6301fbd8e52a
sunabh07 <127098948+sunabh07@users.noreply.github.com>
2023-04-08 23:56:50 +0530

    array in classes

---
+++ arrayinclass.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class employee{
+    int id;
+    int salary;
+    public:
+    void setid(){
+        cout<<"Enter the id"<<endl;
+        cin>>id;
+    }
+    void getid(){
+        cout<<"The id of the employee is " << id <<endl;
+    }
+
+
+};
+int main(){
+    employee a[4];
+    for(int i=0;i<4;i++){
+        a[i].setid();
+        a[i].getid();
+    }
+    return 0;
+}
+
